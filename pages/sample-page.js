98c871d6312f05a3575b49815e76d483645275ff
Blaine OBrien <blaineo@gmail.com>
2018-09-07 12:05:08 -0700

    chore( version bumps for react, next, and standard as well as introduction of Grid compoenents )

--- pages/sample-page.js
+++ pages/sample-page.js
@@ -1,9 +1,17 @@
 import Link from 'next/link'
 import PageTemplate from '../lib/components/PageTemplate'
+import {Row, Column} from '../lib/components/Grid'
 
 const SamplePage = () =>
   <PageTemplate classNames='home'>
-    <Link href='/with-data/tester' prefetch><a>Click for a sample with data request</a></Link>
+    <Row>
+      <Column columns='3'>
+        test
+      </Column>
+      <Column columns='4'>
+        <Link href='/with-data/tester' prefetch><a>Click for a sample with data request</a></Link>
+      </Column>
+    </Row>
   </PageTemplate>
 
 export default SamplePage
