a41bd8d18d99f5b18ba467909a62f45b898824bc
Blaine OBrien <blaineo@gmail.com>
2018-09-07 12:35:46 -0700

    chore( added skip col classes to the column component )

--- pages/sample-page.js
+++ pages/sample-page.js
@@ -5,10 +5,7 @@ import {Row, Column} from '../lib/components/Grid'
 const SamplePage = () =>
   <PageTemplate classNames='home'>
     <Row>
-      <Column columns='3'>
-        test
-      </Column>
-      <Column columns='4'>
+      <Column columns='4' skip='4'>
         <Link href='/with-data/tester' prefetch><a>Click for a sample with data request</a></Link>
       </Column>
     </Row>
